401ef2b47f015f6f297115be77d6ad4002bca10f
alexiscaspell <alexiscaspell@gmail.com>
2016-05-28 22:19:57 -0300

    Agrego funcionalidades a DataBase

--- WindowsFormsApplication1/Clases/DataBase.cs
+++ WindowsFormsApplication1/Clases/DataBase.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace WindowsFormsApplication1.Clases
 {
@@ -53,6 +54,62 @@ namespace WindowsFormsApplication1.Clases
             conexion.Close();
         }
 
+        private SqlDataReader getDataReader(string consulta,char tipoConsulta,List<SqlParameter> parametros)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandText = consulta;
+
+            switch (tipoConsulta)
+            {
+                case 'T':
+                    comando.CommandType = CommandType.Text;
+                    break;
+                /*case 'D':
+                    comando.CommandType = CommandType.TableDirect;
+                    break;*///Lo comento porque por ahora no se cuando se usaria
+                case 'P':
+                    comando.CommandType = CommandType.StoredProcedure;
+                    break;
+            }
+
+            foreach (SqlParameter parametro in parametros)
+            {
+                comando.Parameters.Add(parametro);
+            }
+
+            return comando.ExecuteReader();
+        }
+
+        public void agregarParametro(List<SqlParameter> lista, string parametro, object valor)
+        {
+            lista.Add(new SqlParameter(parametro, valor));
+        }
+
+        public SqlDataReader ejecutarStoredProcedure(string storedProcedure,List<SqlParameter> parametros)
+        {
+            return getDataReader(storedProcedure, 'P', parametros);
+        }
+
+        public SqlDataReader ejecutarConsulta(string consulta, List<SqlParameter> parametros)
+        {
+            return getDataReader(consulta, 'T', parametros);
+        }
+
+        public SqlDataReader ejecutarConsulta(string consulta)
+        {
+            return getDataReader(consulta, 'T', new List<SqlParameter>());
+        }
+
+        public object getValue(SqlDataReader dataReader,string nombreCampo)//hay que castear al valor pedido
+        {
+            return dataReader[nombreCampo];
+        }
+
+        public bool contieneFilas(SqlDataReader reader)
+        {
+            return reader.HasRows;
+        }
 
     }
 }
