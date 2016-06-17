3ed3e2207146a4dd57f25e4762179d79f43db63a
alexiscaspell <alexiscaspell@gmail.com>
2016-06-17 19:13:21 -0300

    Merge commit

--- WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.cs
+++ WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.cs
@@ -21,5 +21,10 @@ namespace WindowsFormsApplication1.Listado_Estadistico
         {
             inicializarFormulario();
         }
+
+        private void inicializarFormulario()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
