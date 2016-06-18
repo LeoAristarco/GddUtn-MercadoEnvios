8b2d24deaf60676b51fd6b0b14481184ef5a6377
alexiscaspell <alexiscaspell@gmail.com>
2016-06-17 22:29:13 -0300

    Hago algo de la logica de listado estadistico

--- WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.cs
+++ WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.cs
@@ -12,8 +12,16 @@ namespace WindowsFormsApplication1.Listado_Estadistico
 {
     public partial class ListadoEstadistico : Form
     {
-        public ListadoEstadistico()
+        private const string VENDEDORES_MAS_VENDIDO = "Vendedores Con mas productos no vendidos";
+        private const string CLIENTES_MAS_COMPRAS = "Clientes con mas compras";
+        private const string VENDEDORES_MAS_FACTURAS = "Vendedores con mas facturas";
+        private const string VENDEDORES_MAYOR_FACTURA = "Vendedores con mayor facturacion";
+        private const string VACIO = "Ninguno";
+        private Form formAnterior;
+
+        public ListadoEstadistico(Form formAnterior)
         {
+            this.formAnterior = formAnterior;
             InitializeComponent();
         }
 
@@ -23,6 +31,86 @@ namespace WindowsFormsApplication1.Listado_Estadistico
         }
 
         private void inicializarFormulario()
+        {
+            tipoListado.Items.Add(VACIO);
+            tipoListado.Items.Add(VENDEDORES_MAS_VENDIDO);
+            tipoListado.Items.Add(CLIENTES_MAS_COMPRAS);
+            tipoListado.Items.Add(VENDEDORES_MAS_FACTURAS);
+            tipoListado.Items.Add(VENDEDORES_MAYOR_FACTURA);
+
+            for (int i = 0; i < 5; i++)
+            {
+                trimestre.Items.Add(i);
+            }
+
+            trimestre.SelectedIndex = 0;
+            tipoListado.SelectedIndex = 0;
+
+            tablaEstadistica.Rows.Clear();
+            tablaEstadistica.Columns.Clear();
+        }
+
+        private void volverButton_Click(object sender, EventArgs e)
+        {
+            formAnterior.Show();
+            Close();
+        }
+
+        private void anio_TextChanged(object sender, EventArgs e)
+        {
+            if (anio.Text.Length>4)
+            {
+                anio.Text = anio.Text.Remove(anio.Text.Length - 1);
+            }
+
+            trimestre.Enabled = anio.Text.Length == 4;
+        }
+
+        private void trimestre_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tipoListado.Enabled = (int)trimestre.SelectedItem != 0;
+        }
+
+        private void limpiarBoton_Click(object sender, EventArgs e)
+        {
+            inicializarFormulario();
+        }
+
+        private void tipoListado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (tipoListado.SelectedItem.ToString())
+            {
+                case VENDEDORES_MAS_VENDIDO:
+                    iniciarVenderoresMasVendido();
+                    break;
+                case VENDEDORES_MAS_FACTURAS:
+                    iniciarVenderoresMasFacturas();
+                    break;
+                case VENDEDORES_MAYOR_FACTURA:
+                    iniciarVenderoresMayorFactura();
+                    break;
+                case CLIENTES_MAS_COMPRAS:
+                    iniciarClientesMasCompras();
+                    break;
+            }
+        }
+
+        private void iniciarClientesMasCompras()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void iniciarVenderoresMayorFactura()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void iniciarVenderoresMasFacturas()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void iniciarVenderoresMasVendido()
         {
             throw new NotImplementedException();
         }
