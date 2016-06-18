b592b455ea71d01ba8f1bce814dbc2a3bde8689a
alexiscaspell <alexiscaspell@gmail.com>
2016-06-18 11:48:08 -0300

    Agrego stored del repo trucho del lider

--- Lider_SQLQuery1.sql
+++ Lider_SQLQuery1.sql
@@ -32,6 +32,41 @@ end
 
 go
 
+create procedure st_buscar_publicaciones_ULTIMA_PAGINA
+@descripcion nvarchar(255) = null,
+@rubroId nvarchar(255)= null,
+@ultimaPagina int output
+AS
+begin
+declare @paginas int
+
+ select @paginas=count(publicaciones)
+ from(
+      select (row_number() over (order by id_publicacion desc) ) as publicaciones
+      from PUBLICACION
+      inner join VISIBILIDAD on id_visibilidad = visibilidad
+      inner join ESTADO_PUBLICACION on id_estado = estado_publicacion
+      inner join TIPO_PUBLICACION on id_tipo = tipo_publicacion
+      inner join RUBRO on id_rubro = rubro
+      where (descripcion like '%' + @descripcion + '%') and 
+      estado_nombre <> 'BORRADOR' and estado_nombre <> 'FINALIZADO' and
+      (id_rubro = @rubroId OR @rubroId IS NULL)
+      
+      ) gg_vieja
+
+	  if((@paginas%10)<>0)
+	  begin
+	       set @ultimaPagina = (@paginas/10)+1
+	  end
+	  else
+	  begin
+	       set @ultimaPagina = (@paginas/10)
+	  end
+
+end
+
+go
+
 -- Procedimientos que usa el store st_insertarCompraSubasta
 create procedure st_actualizar_Estado_Publicacion_a_Finalizado
 @publicacion numeric(10,0),
