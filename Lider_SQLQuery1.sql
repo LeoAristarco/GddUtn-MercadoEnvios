eeb3d77dd1aa3cbc8ec31305c35b2dc1de7cdf46
LeoAristarco <chucky_92@hotmail.com.ar>
2016-06-18 19:51:29 -0300

    agrego st_actualizar_publicaciones_vencidas

--- Lider_SQLQuery1.sql
+++ Lider_SQLQuery1.sql
@@ -247,7 +247,7 @@ end
 
 go
 
-alter procedure st_ultimas5compras(@id_usuario numeric(10,0))
+create procedure st_ultimas5compras(@id_usuario numeric(10,0))
 as begin  
 	  select top 5 descripcion,calif_estrellas
 	  from COMPRA
@@ -394,16 +394,66 @@ having 0 =(select count(*) from ITEM_FACTURA
 
 --Al iniciar la aplicación se tiene que barrer la base de datos para saber que publicaciones
 --estan vencidas, a las q estan vencidas hay que finalizarlas y cerrar la factura
-CREATE PROCEDURE st_actualizar_publicaciones_vencidas
-AS
-DECLARE @fecha datetime
+--Las subasta vencidas hay q hacer lo mismo, y tambien insertar esa oferta como compra
+create procedure st_actualizar_publicaciones_vencidas
+@fechaDelSistema datetime
 
-UPDATE PUBLICACION  
-	SET estado_publicacion = 4 --finalizado
-	WHERE	
-			year(fecha_vencimiento) < 2016 AND
-			estado_publicacion <> 1 AND --distinto de borrador
-			( select factura_fecha  -- todavia no se facturo
-			          from FACTURA
-			          where factura = id_factura) is null
+AS BEGIN
+declare @id_publicacion numeric(10,0), @tipo_publicacion numeric(10,0), @factura numeric(10,0),@precio numeric(10,2),
+@descripcion nvarchar(255),
+@ofertante numeric(10,0),@precio_envio numeric(10,2)
+
+declare miCursor cursor
+ for select id_publicacion,tipo_publicacion,
+            factura,precio,descripcion 
+ FROM PUBLICACION
+ where  year(fecha_vencimiento) < 2016 and
+ estado_publicacion <> 1 AND --distinto de borrador
+ estado_publicacion <> 4     --distinto de finalizado
+ 
+
+open miCursor
+
+fetch next from miCursor into @id_publicacion,@tipo_publicacion,@factura,@precio,@descripcion
+
+while @@fetch_status=0
+begin
+       if(@tipo_publicacion =1)
+       begin
+             UPDATE PUBLICACION  
+	       SET estado_publicacion = 4 --finalizado
+	       where id_publicacion= @id_publicacion
+       
+	         UPDATE FACTURA  
+	       SET factura_fecha = @fechaDelSistema --cierro la factura
+	       where id_factura =@factura
+       end
+       else
+       begin
+             UPDATE PUBLICACION  
+	       SET estado_publicacion = 4 --finalizado
+	       where id_publicacion= @id_publicacion
+       
+	         UPDATE FACTURA  
+	       SET factura_fecha = @fechaDelSistema --cierro la factura
+	       where id_factura =@factura
+
+	       select @ofertante=ofertante,@precio_envio=precio_envio
+	       from OFERTA
+	       where publicacion =@id_publicacion and
+	       monto_ofertado = @precio
+
+	       exec st_insertarCompraSubasta @ofertante,@id_publicacion ,@fechaDelSistema, @precio, 
+                              1 ,@precio_envio ,@factura ,@descripcion 
+       end
+
+       fetch next from miCursor into @id_publicacion,@tipo_publicacion,@factura,@precio    
+end
+
+close miCursor
+
+deallocate miCursor
+
+end
+	
 			
