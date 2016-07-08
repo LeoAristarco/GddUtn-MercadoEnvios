1f08c71d9f80a88021c1486a9677b24640215984
brianwolf <brian.d.lobo@gmail.com>
2016-07-08 18:08:39 -0300

    se pueden ver los roles de los usuarios

--- WindowsFormsApplication1/ABM Rol/RolesUsuarioDlg.cs
+++ WindowsFormsApplication1/ABM Rol/RolesUsuarioDlg.cs
@@ -9,28 +9,51 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApplication1.Clases;
 
+
 namespace WindowsFormsApplication1.ABM_Rol
 {
     public partial class RolesUsuarioDlg : Form
     {
         private Usuario usuario;
+        private RolRepository repo;
+        private List<Rol> rolesDelUsuario;
+        private List<Rol> rolesDelSistema;
 
         public RolesUsuarioDlg(Usuario usuario)
         {
             InitializeComponent();
 
             this.usuario = usuario;
+            repo = new RolRepository();
+
+            cargarDatosDeUsuario();
         }
 
         private void cargarDatosDeUsuario()
         {
             txtIdUser.Text = usuario.id.ToString();
             txtUsername.Text = usuario.nick;
+
+            rolesDelUsuario = repo.obtenerRolesDe(usuario);
+            rolesDelSistema = repo.TodosLosRolesDelSistema();
+
+            //carga de la lista de checkboc
+            foreach (Rol rol in rolesDelSistema)
+            {
+                cblRoles.Items.Add(rol.nombre, rolesDelUsuario.Any(x => x.id == rol.id));
+            }
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void cblRoles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+
--- WindowsFormsApplication1/Clases/RolRepository.cs
+++ WindowsFormsApplication1/Clases/RolRepository.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using WindowsFormsApplication1.Clases;
 
+
 namespace WindowsFormsApplication1.ABM_Rol
 {
-    internal class RolRepository:Repository
+    internal class RolRepository : Repository
     {
         public List<Rol> obtenerRolesDe(Usuario usuario)
         {
             List<Rol> roles = new List<Rol>();
-            
+
             string procedimiento = "OBTENER_ROLES_POR_ID_USUARIO";
 
             SqlParameter pIdSUsuario = new SqlParameter("@id_usuario", usuario.id);
             List<SqlParameter> parametros = new List<SqlParameter> { pIdSUsuario };
-            
+
             List<Dictionary<string, object>> listaBD = db.ejecutarStoredProcedure(procedimiento, parametros);
 
             foreach (Dictionary<string, object> dic in listaBD)
@@ -27,6 +28,24 @@ namespace WindowsFormsApplication1.ABM_Rol
             return roles;
         }
 
+        public List<Rol> TodosLosRolesDelSistema()
+        {
+            List<Rol> roles = new List<Rol>();
+
+            string sql = "select * from ROL";
+
+            List<Dictionary<string, object>> listaBD = db.ejecutarConsulta(sql);
+
+            foreach (Dictionary<string, object> dic in listaBD)
+            {
+                Rol rol = new Rol(Convert.ToInt64(dic["id_rol"]), (string)dic["rol_nombre"], (bool)dic["habilitado"]);
+                roles.Add(rol);
+            }
+
+            return roles;
+        }
+
+
         public void eliminarRolDe(Usuario user, Rol rolSeleccionado)
         {
             throw new NotImplementedException();
@@ -36,7 +55,7 @@ namespace WindowsFormsApplication1.ABM_Rol
         {
             List<Usuario> usuarios = new List<Usuario>() { };
 
-            string procedimiento = "OBTENER_USUARIOS_PARA_ABM_ROL";            
+            string procedimiento = "OBTENER_USUARIOS_PARA_ABM_ROL";
             List<SqlParameter> parametros = new List<SqlParameter> { };
 
             List<Dictionary<string, object>> listaBD = db.ejecutarStoredProcedure(procedimiento, parametros);
@@ -44,8 +63,8 @@ namespace WindowsFormsApplication1.ABM_Rol
             foreach (Dictionary<string, object> dic in listaBD)
             {
                 Usuario user = new Usuario();
-                    user.id = Convert.ToInt64(dic["id_usuario"]);
-                    user.nick = (string)dic["nick"];
+                user.id = Convert.ToInt64(dic["id_usuario"]);
+                user.nick = (string)dic["nick"];
 
                 usuarios.Add(user);
             }
@@ -54,3 +73,7 @@ namespace WindowsFormsApplication1.ABM_Rol
         }
     }
 }
+
+
+
+
