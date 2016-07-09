f640219734e6c4170ef21872d703f907ff13a24f
alexiscaspell <alexiscaspell@gmail.com>
2016-07-09 12:58:50 -0300

    Updateo csproj

--- WindowsFormsApplication1/Facturas/FacturasRealizadas.cs
+++ WindowsFormsApplication1/Facturas/FacturasRealizadas.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApplication1.Login;
 
 namespace WindowsFormsApplication1.Facturas
 {
     public partial class FacturasRealizadas : Form
     {
+        private SeleccionFuncionalidades seleccionFuncionalidades;
+
         public FacturasRealizadas()
         {
             InitializeComponent();
+        }
+
+        public FacturasRealizadas(SeleccionFuncionalidades seleccionFuncionalidades)
+        {
+            this.seleccionFuncionalidades = seleccionFuncionalidades;
         }
     }
 }
